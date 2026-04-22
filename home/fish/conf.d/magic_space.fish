9cc164eb6e3e791111da10c54a0f19a9a5af5280
Chad Norvell <chadnorvell@pm.me>
2026-04-22 10:12:27 -0700

    fix magic space abbr expansion

--- home/fish/conf.d/magic_space.fish
+++ home/fish/conf.d/magic_space.fish
@@ -1,5 +1,8 @@
 # Enable appending ' ! <new command>' to replace original command
 function _magic_space
+    # Reimplement the default abbr expansion on space
+    commandline -f expand-abbr
+
     set -l buf (commandline -b)
 
     # Match (original command) (args) ! (new command)
