efe6874f39e28b3a34e71a6dc90c97bf54af9dc5
Chad Norvell <chad@chadnorvell.org>
2026-04-25 17:08:15 -0700

    add iterm2 settings

--- home/darwin/darwin.nix
+++ home/darwin/darwin.nix
@@ -3,6 +3,8 @@ let
   ln' = pkgs.lib'.ln';
 in
 {
+  imports = [ ./iterm2.nix ];
+
   programs.git.settings.core.sshCommand = "/usr/bin/ssh";
 
   programs.ssh.extraConfig = ''
@@ -21,4 +23,67 @@ in
     "90-local.fish"
     "99-nix.fish"
   ];
+
+  programs.iterm2 = {
+    enable = true;
+    settings.appearance.theme = "minimal";
+
+    profiles = [
+      {
+        name = "Default";
+        default = true;
+
+        font = {
+          normal = "IosevkaNFM 16";
+          antiAlias = true;
+          brightenBold = true;
+        };
+
+        colors = {
+          foreground = "#d8dee9";
+          background = "#242933";
+
+          black = {
+            normal = "#000000";
+            bright = "#808080";
+          };
+
+          red = {
+            normal = "#ec5f66";
+            bright = "#f97b58";
+          };
+
+          green = {
+            normal = "#99c794";
+            bright = "#acd1a8";
+          };
+
+          yellow = {
+            normal = "#f9ae58";
+            bright = "#fac761";
+          };
+
+          blue = {
+            normal = "#6699cc";
+            bright = "#85add6";
+          };
+
+          magenta = {
+            normal = "#c695c6";
+            bright = "#d8b6d8";
+          };
+
+          cyan = {
+            normal = "#5fb4b4";
+            bright = "#82c4c4";
+          };
+
+          white = {
+            normal = "#f7f7f7";
+            bright = "#ffffff";
+          };
+        };
+      }
+    ];
+  };
 }
